5980342cbaf9243266da3305a24656219e492300
elliot! <benjikrolfanpageuwu@gmail.com>
2025-11-26 16:44:24 +0000

    Add project files.

---
+++ GurtDeco/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace GurtDeco
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class Constants
+    {
+        public const string GUID = "com.elliot.gorillatag.gurtdeco";
+        public const string Name = "GurtDeco";
+        public const string Version = "1.0.0";
+    }
+}
---
+++ GurtDeco/Plugin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using BepInEx;
+using GurtDeco.Tools;
+using UnityEngine;
+
+namespace GurtDeco
+{
+    [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance { get; private set; }
+        private GameObject _fellasPrefab;
+        private bool _initialized;
+
+        private void Start()
+        {
+            Instance = this;
+            GorillaTagger.OnPlayerSpawned(OnPlayerSpawned);
+        }
+
+        private void OnPlayerSpawned()
+        {
+            if (_initialized) return;
+            _initialized = true;
+            _ = SetupElligurt();
+        }
+
+        private async Task SetupElligurt()
+        {
+            try
+            {
+                _fellasPrefab = await AssetLoader.LoadAsset<GameObject>("GurtDeco");
+                if (_fellasPrefab == null)
+                {
+                    Debug.LogError("[Fellas] Failed to load the silly waving fella");
+                    return;
+                }
+
+                var fellasInstance = Instantiate(_fellasPrefab);
+                fellasInstance.SetActive(true);
+                fellasInstance.transform.position = new Vector3(-65.1718f, 12.051f, - 79.7311f);
+                fellasInstance.transform.rotation = Quaternion.Euler(355.7651f, 206.2997f, 359.3513f);
+                fellasInstance.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            }
+
+            catch (Exception ex)
+            {
+                Debug.LogError("[Fellas] Error setting up the siliest: " + ex);
+            }
+        }
+    }
+}
